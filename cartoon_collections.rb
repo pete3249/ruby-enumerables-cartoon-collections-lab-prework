5c8d8c539cb797a2bba51e50247fd99a83a0b3e5
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-06 15:22:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array) #defining method and argument
 end
   
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array) #Defining method and accepting an aray of strings
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
