528f299e83c273ad81fac352d662eeb0ad24729b
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-06 15:20:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(array) #defining method and argument
  array.any? {|item| item.length > 4}
 end
   
-  
+
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
