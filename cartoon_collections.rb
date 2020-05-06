6be1cdd2310177c913a4c96be8b3079c4e2d25ee
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-06 15:12:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,9 @@ end #closing method
 def long_planeteer_calls(array) #defining method and argument
   array.each do |item| #iterate over each item in the array
     if item.length > 4 #if the word is longer than 4 characters
-      returns "True"
+      puts "True"
     else #if the word is not longer than 4 characters
-      returns "False"
+      puts "False"
     end
   end
   array.include? ("True")
