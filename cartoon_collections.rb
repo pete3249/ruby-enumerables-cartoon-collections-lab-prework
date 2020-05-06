1ccd9f0e481520496f438dc87d69f1e1bf02ae6f
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-06 14:29:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,9 +13,7 @@ def summon_captain_planet(array) #defining method and argument
 end #closing method
 
 def long_planeteer_calls(array) #defining method and argument
-  array.include?     # Your code here
-      
-      
+  array.include? {|string| string.length > 4}    # Your code here
 end
 
 def find_the_cheese# code an argument here
