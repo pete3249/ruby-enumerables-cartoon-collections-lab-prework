52ec907772add73e0cc196834704be9bd209a495
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-06 14:59:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,20 +21,6 @@ def long_planeteer_calls(array) #defining method and argument
     end
   end
 end
-    
-  
-  
-  
-  
-  array.each do |item| #iterate through each item in an array
-    array[item]
-    
-  
-
-  array.include? ()
-  if array.include? {
-  array.include? {|call| call.to_s.length > 4} #
-end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
