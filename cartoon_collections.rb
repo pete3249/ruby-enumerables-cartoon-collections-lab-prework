94a9063be826fa15eaa51e417316071657d00381
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-06 15:43:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,13 +19,16 @@ end
 
 def find_the_cheese(array) #Defining method and accepting an aray of strings
   cheese_types = ["cheddar", "gouda", "camembert"]
-  count = 0
-  if array[count].include? ("cheddar")
-   puts "cheddar"
-    else array[count].include? ("gouda")
-    puts "gouda"
-    elsif array[count].include? ("camembert")
-    puts "camembert"
-  end
-end
-end
+end  
+  
+  
+  
+  
+  #count = 0
+  #if array[count].include? ("cheddar")
+  # puts "cheddar"
+   # else array[count].include? ("gouda")
+   # puts "gouda"
+   # elsif array[count].include? ("camembert")
+   # puts "camembert"
+ 
