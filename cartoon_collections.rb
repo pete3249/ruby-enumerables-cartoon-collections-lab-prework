c9e037908fa8ec0951a63f34f7d844c21995777e
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-05-06 15:16:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,6 +24,7 @@ def long_planeteer_calls(array) #defining method and argument
   array.include? ("True")
 end
   
+  array.length
   
 def find_the_cheese# code an argument here
   # the array below is here to help
